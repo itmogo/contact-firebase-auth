6cf6384ef573bc5407eea3a65625ea11e07ca8ee
edward frempong <eddysoftwarengineer@gmail.com>
2021-10-06 19:14:20 +0000

    added settings config

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+export default function Settings() {
+  return (
+    <div>
+     <h1>Settings for logged in users</h1>
+
+    </div>
+  );
+}
+
+
+export default Settings;
